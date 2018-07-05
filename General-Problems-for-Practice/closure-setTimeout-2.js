8c39b476c35a83d87ba83936d9d2c3db4d098466
Rohan Paul <rohanpaul2@gmail.com>
2018-07-05 19:03:28 +0530

    closures use case

--- General-Problems-for-Practice/closure-setTimeout-2.js
+++ General-Problems-for-Practice/closure-setTimeout-2.js
@@ -14,7 +14,7 @@ Index: 4, element : undefined
 
 */
 
-/* Explanantion -
+/* Explanation -
 
 The reason for this is because the setTimeout function creates a function (the closure) that has access to its outer scope, which is the loop that contains the index i. After 3 seconds go by, the function is executed and it prints out the value of i, which at the end of the loop is at 4 because it cycles through 0, 1, 2, 3, 4 and the loop finally stops at 4.arr[4] does not exist, which is why you get undefined.
 
@@ -34,7 +34,7 @@ for (let i = 0; i < arr1.length; i++) {
 }
 
 
-// SOLUTION - 3 - To print all number from 0..4 after 1000 ms
+// SOLUTION - 2 - To print all number from 0..4 after 1000 ms
 
 const arr2 = [10, 12, 15, 21];
 
