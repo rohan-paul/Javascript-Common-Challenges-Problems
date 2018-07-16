070d896cdc906f20c50899356e9db1953b40644b
Rohan Paul <rohanpaul2@gmail.com>
2018-07-16 18:26:33 +0530

    more alternative sol to chek if string has only numbers

--- Regexp/check-if-String-has-only-numbers.js
+++ Regexp/check-if-String-has-only-numbers.js
@@ -1,9 +1,22 @@
 // I wanna make sure the input contains ONLY digits and no other letters. Write a function to return true if all the chars of the given string are only digits not even + or -
 
-ifStringIsOnlyNumeric = str => str.match(/^[0-9]+$/) !== null
+let str1 = "88kl85"
+let str2 = "8885"
 
-console.log(ifStringIsOnlyNumeric("88kl85"));   // => false
-console.log(ifStringIsOnlyNumeric("8885"));     // => false
+let re = /^[0-9]+$/;
+
+
+// SOLUTION-1
+ifStringIsOnlyNumeric = str => str.match(re) !== null
+
+// console.log(ifStringIsOnlyNumeric(str1));   // => false
+// console.log(ifStringIsOnlyNumeric(str2));     // => true
+
+// SOLUTION-2
+ifStringIsOnlyNumeric2 = str => re.test(str)
+
+// console.log(ifStringIsOnlyNumeric2(str1));   // => false
+// console.log(ifStringIsOnlyNumeric2(str2));     // => true
 
 /* match() method - match() method retrieves the matches when matching a string against a regular expression.
 
@@ -21,10 +34,50 @@ So, by wrapping the regexp with a '^' and '$' I am making sure, that inside the 
 
 */
 
-let str1 = "88kl85"
-let str2 = "8885"
 
-let re = /^[0-9]+$/;
+// console.log(str1.match(re));  // => null
+// console.log(str2.match(re));  // => [ '8885', index: 0, input: '8885' ]
 
-console.log(str1.match(/^[0-9]+$/));  // => null
-console.log(str2.match(re));  // => [ '8885', index: 0, input: '8885' ]
+// console.log(re.test(str1));   // =>  false
+// console.log(re.test(str2));   // => true
+
+// SOLUTION-3 -  to allow both comma and point decimal
+
+let re2 = /^\d+[\.\,]?\d+$/
+
+let str3 = "88,85";
+
+let str4 = "88.85";
+
+ifStringIsOnlyNumeric3 = str => re2.test(str);
+
+console.log(ifStringIsOnlyNumeric3(str1));    // => false
+console.log(ifStringIsOnlyNumeric3(str2));    // => true
+console.log(ifStringIsOnlyNumeric3(str3));    // => true
+console.log(ifStringIsOnlyNumeric3(str4));    // => true
+
+/*
+\d	=>> Matches a digit character. Equivalent to [0-9].
+
+\	=>> Marks the next character as a special character, a literal, a back-reference, or an octal escape. For example, 'n' matches the character "n". '\n' matches a newline character. The sequence '\\' matches "\" and "\(" matches "(".
+
+so, in above I am escaping a dot character and a comma character
+
+?  =>>  When this character immediately follows any of the other quantifiers (*, +, ?, {n}, {n,}, {n,m}), the matching pattern is non-greedy. A non-greedy pattern matches as little of the searched string as possible, whereas the default greedy pattern matches as much of the searched string as possible. For example, in the string "oooo", 'o+?' matches a single "o", while 'o+' matches all 'o's.
+
+Meaning in above, I only want to match a single dot or a single comma
+
+*/
+
+// Without using regexp - 48 and 57 are the char codes for "0" and "9", respectively.
+
+ifStringIsOnlyNumeric4 = str => {
+  for (let i = 0; i < str.length - 1; i++) {
+    let char = str.charCodeAt(i);
+    if ( char < 48 || char > 57 )return false;
+  }
+  return true;
+}
+
+console.log(ifStringIsOnlyNumeric4(str1));   // => false
+console.log(ifStringIsOnlyNumeric4(str2));     // => true
