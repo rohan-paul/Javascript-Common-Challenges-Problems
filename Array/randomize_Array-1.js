6c96037018a903d8fea433fb5f41ed97714f6805
Rohan Paul <rohanpaul2@gmail.com>
2018-07-24 19:57:04 +0530

    more alt solution to shuffling array

--- Array/randomize_Array-1.js
+++ Array/randomize_Array-1.js
@@ -14,40 +14,16 @@ shuffleArr = arr => {
     while (len) {
 
         // generate an index which is between 0 and (currentIndex - 1)
-        randomIndex = Math.floor(Math.random() * len);
-
-        // For the first iteration itselft, before the shuffling, ie. before swapping, I have to reduce currentIndex by 1 as currentIndex is the length of the array, and the max index of an array is ( array.length - 1)
-
-        len--;
+        randomIndex = Math.floor(Math.random() * len--);
 
         [arr[len], arr[randomIndex] ] = [ arr[randomIndex], arr[len] ]
-
     }
     return arr;
 }
-/* https://stackoverflow.com/questions/21483667
 
-The Math.random() function returns a floating-point, pseudo-random number in the range [0, 1) that is, from 0 (inclusive) up to but not including 1 (exclusive). That is, it returns only one interval, [0,1).
+let myArr1 = [1, 2, 3, 4, 5]
 
-That is, 0 <= Math.random() < 1. The multiplier is what extends this to include the specified number of intervals.
-
-    Here are two example randomly generated numbers:
-
-    Math.random() // 0.011153860716149211
-    Math.random() // 0.9729151880834252
-
-    Because of this, when we multiply our randomly generated number by another number, it will range from 0 to a maximum of 1 lower than the number being multiplied by
-
-    Because, The Math.floor() function returns the largest integer less than or equal to a given number.
-
-    Effectively, Math.floor() simply removes the decimal places rather than rounding the number (that is to say, 0.999 becomes 0 when processed with Math.floor(), not 1)).
-
-    Math.floor(0.011153860716149211 * 5) // 0
-    Math.floor(0.9729151880834252 * 5)   // 4 */
-
-let myArr = [1, 2, 3, 4, 5]
-
-// console.log(shuffleArr(myArr))
+console.log(shuffleArr(myArr1))
 
 /* The much less optimized version.
 Pick a random element from the array (in [0, n - 1]) and then check if you’ve shuffled that element already. This works, but it becomes increasingly slow as the remaining elements dwindle; I will keep randomly picking elements that have already been shuffled.
@@ -71,4 +47,47 @@ shuffleArr_Slow = arr => {
     }
     return shuffledArr;
 }
-console.log(shuffleArr_Slow(myArr));
+let myArr2 = [1, 2, 3, 4, 5]
+console.log(shuffleArr_Slow(myArr2));
+
+//Same algo as above with splice()
+shuffleArr_Slow_2 = arr => {
+
+    let len = arr.length;
+    let shuffledArr = [];
+    let randNum;
+
+    while (len) {
+
+        // Because here, I am not separately decrementing the array within any further if or for loop. So, have to take care of decrementing the random number generation part withing the Math.random() function invocation itself
+        randNum = Math.floor(Math.random() * len--);
+
+        // noting splice() mutates the array
+        shuffledArr.push(arr.splice(randNum, 1)[0]);
+    }
+    return shuffledArr;
+}
+
+let myArr3 = [1, 2, 3, 4, 5]
+
+console.log(shuffleArr_Slow_2(myArr3))
+
+/* https://stackoverflow.com/questions/21483667
+
+The Math.random() function returns a floating-point, pseudo-random number in the range [0, 1) that is, from 0 (inclusive) up to but not including 1 (exclusive). That is, it returns only one interval, [0,1).
+
+That is, 0 <= Math.random() < 1. The multiplier is what extends this to include the specified number of intervals.
+
+    Here are two example randomly generated numbers:
+
+    Math.random() // 0.011153860716149211
+    Math.random() // 0.9729151880834252
+
+    Because of this, when we multiply our randomly generated number by another number, it will range from 0 to a maximum of 1 lower than the number being multiplied by
+
+    Because, The Math.floor() function returns the largest integer less than or equal to a given number.
+
+    Effectively, Math.floor() simply removes the decimal places rather than rounding the number (that is to say, 0.999 becomes 0 when processed with Math.floor(), not 1)).
+
+    Math.floor(0.011153860716149211 * 5) // 0
+    Math.floor(0.9729151880834252 * 5)   // 4 */
