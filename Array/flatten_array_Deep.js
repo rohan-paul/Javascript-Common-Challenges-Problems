75924e10d01141b77b55c3adda320ec01e4008d7
Rohan Paul <rohanpaul2@gmail.com>
2018-07-31 22:56:58 +0530

    flatten array alt solution

--- Array/flatten_array_Deep.js
+++ Array/flatten_array_Deep.js
@@ -24,9 +24,8 @@ console.log(deepFlatten2(myArr1)); // => [ 1, 2, 3, 4, 5 ]
 
 /*My Note - Explanation of why an empty array passed as the second argument to the reduce function which is the accumulator - The code breaks without it, giving arr.reduce is not a function.
 
-The empty array [] is the starting accumulator value for the reduce function, the initial value. In this case it's the value of flat in the first call to the anonymous function passed to reduce. If it is not specified, then the first call to reduce binds the first value out of the array to flat, which would eventually result in 1 being bound to flat in both the examples. 1.concat is not a function
+The empty array [] is the starting accumulator value for the reduce function, the initial value. In this case it's the value of flat in the first call to the anonymous function passed to reduce. If it is not specified, then the first call to reduce binds the first element out of the array-argument given to deepFlatten2() function to flat, which in the above test-case would result in 1 (the first element of the argument) being bound to flat in both the examples. 1.concat is not a function
 
-In this case it's the value of flat in the first call to the anonymous function passed to reduce. If it is not specified, then the first call to reduce binds the first value out of the array to flat
 */
 
 // SOLUTION-3 - huge (e.g. 200 000 elements) arrays and also works on nested arrays
