f4188457e62824d0abebf7696bc8b4ae61a324b0
Rohan Paul <rohanpaul2@gmail.com>
2018-08-05 17:29:40 +0530

    title to Camel case

---
+++ String/toLocalString-Application.js
@@ -0,0 +1,6 @@
+var number = 123456.789;
+
+// India uses thousands/lakh/crore separators
+console.log(number.toLocaleString('en-IN'));  // => 12,345,678.896
+
+// console.log(number.toLocaleString())
