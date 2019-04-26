9af459eeecbda577300035dd3549290e1de04f32
Rohan Paul <rohanpaul2@gmail.com>
2019-04-26 20:59:44 +0530

    convert objects to array of objects

--- Objects-Master-List-of-Problems/object-accessing-keys-values.js
+++ Objects-Master-List-of-Problems/object-accessing-keys-values.js
@@ -1,31 +1,30 @@
 // Codes to access object keys and values in JS
 
 let obj = {
-	"a": 1,
-	"b": 2,
-    "c": 3
-}
+  a: 1,
+  b: 2,
+  c: 3
+};
 
-console.log(obj.a);  // => 1
+console.log(obj.a); // => 1
 
-console.log(obj["a"]);  // => 1
+console.log(obj["a"]); // => 1
 
 // console.log(obj[a] === true); // => ReferenceError: a is not defined
 
 console.log(obj.hasOwnProperty("a")); // => true
 
-console.log(Object.keys(obj));  // => [ 'a', 'b', 'c' ]
+console.log(Object.keys(obj)); // => [ 'a', 'b', 'c' ]
 
-console.log(Object.values(obj));  // => [ 1, 2, 3 ]
+console.log(Object.values(obj)); // => [ 1, 2, 3 ]
 
-console.log('a' in obj); //true
+console.log("a" in obj); //true
 
-console.log('toString' in obj); //true
+console.log("toString" in obj); //true
 
-console.log(obj.hasOwnProperty('a')); //true
+console.log(obj.hasOwnProperty("a")); //true
 
-console.log(obj.hasOwnProperty('toString')); //false
-
+console.log(obj.hasOwnProperty("toString")); //false
 
 /* The above 'obj' will store as its key-value the unique elements of the array (as keys) and a boolean (true for each key). Meaning for any duplicate elements of the given array obj[hasOwnProperty] will return false.
 
@@ -39,15 +38,17 @@ console.log(obj.hasOwnProperty('toString')); //false
   If the key is not yet known when we write the code, then we must use object[key] but be careful, sometimes we need to wrap the key in quotes:
   object["key"] like when the key is not a variable. */
 
- // Write a function to return the key from an object given a value
- // Both Object.keys and Object.values will return me an array, so I can further apply find() or indexOf() methods on them
+// Write a function to return the key from an object given a value
+// Both Object.keys and Object.values will return me an array, so I can further apply find() or indexOf() methods on them
 
-findKeyFromValue1 = (obj, value) => Object.keys(obj).find(key => obj[key] === value);
+findKeyFromValue1 = (obj, value) =>
+  Object.keys(obj).find(key => obj[key] === value);
 
-console.log(findKeyFromValue1(obj, 2));  // => b
+console.log(findKeyFromValue1(obj, 2)); // => b
 
 // Next alternative to find the key given the value
-findKeyFromValue2 = (obj, value) => Object.keys(obj).filter(key => obj[key] === value)[0];
+findKeyFromValue2 = (obj, value) =>
+  Object.keys(obj).filter(key => obj[key] === value)[0];
 
 // console.log(findKeyFromValue2(obj, 2));  // => [ 1, 2, 3 ]
 
@@ -60,7 +61,7 @@ findKeyFromValue2 = (obj, value) => Object.keys(obj).filter(key => obj[key] === 
  D) Finally I am doing keys[indexNo] kind of syntax
  */
 findKeyFromValue3 = (obj, value) => {
-	return Object.keys(obj)[Object.values(obj).indexOf(value)]
-}
+  return Object.keys(obj)[Object.values(obj).indexOf(value)];
+};
 
-console.log(findKeyFromValue3(obj, 2));  // => b
+console.log(findKeyFromValue3(obj, 2)); // => b
