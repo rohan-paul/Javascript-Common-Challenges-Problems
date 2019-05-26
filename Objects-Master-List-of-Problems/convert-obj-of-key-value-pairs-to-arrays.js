f412e9d9b0ca03c3c20ae4a3e30c996e318f78b3
Rohan Paul <rohanpaul2@gmail.com>
2019-05-26 13:09:14 +0530

    deleting some files

---
+++ Objects-Master-List-of-Problems/convert-obj-of-key-value-pairs-to-arrays.js
@@ -0,0 +1,27 @@
+/* Sometimes, you might find that it’s easier to use an array for your list of items. For example, when you are rendering a list. It’s easier to have an array, use a map and create the element in each iteration.
+
+This has an easy solution. Instead of accessing directly the state, use a function. What is also known as a selector.
+
+A selector is a function that takes some state and returns the data you need.
+ */
+
+// object
+const comments = {
+  "1": { id: "1", text: "please add code examples" },
+  "2": { id: "2", text: "examples would be great for this article" },
+  "3": { id: "3", text: "hi there" }
+};
+
+/* But I need the data in the below format
+
+[ { id: '1', text: 'please add code examples' },
+  { id: '2', text: 'examples would be great for this article' },
+  { id: '3', text: 'hi there' } ]
+
+*/
+
+const commentsSelector = commentObj => {
+  return Object.keys(commentObj).map(commentKey => commentObj[commentKey]);
+};
+
+console.log(commentsSelector(comments));
