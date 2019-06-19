e1e1c9affc2bbb600a8711ef7e6fb798715dc457
Rohan Paul <rohanpaul2@gmail.com>
2019-06-19 12:05:51 +0530

    reverse string with python

--- General-JS-Problems/reverse-string-multiple-ways.py
+++ General-JS-Problems/reverse-string-multiple-ways.py
@@ -1,8 +1,19 @@
-""" 1st approach
-This approach is like using the reduce() method in JS.
+''' 1-st
+ using python's builtin reversed function '''
+
+
+def reverse_str(s):
+    return "".join(reversed(s))
+
+
+print(reverse_str('rohan'))
+
+
+""" 2-nd approach
+This approach is like using the Array.reduce() method of  in JS.
 The first iteration makes the str to take up value of 'G'
 The second iteration makes 'str' to be 'eG'
-The thridt iteration makes 'str' to be 'eeG' and so on and so forth.
+The thrird iteration makes 'str' to be 'eeG' and so on and so forth.
 """
 
 
