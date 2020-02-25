8925b2cde2323be2fdf70efa35a3b82d53cdf1b9
Rohan Paul <rohanpaul2@gmail.com>
2020-02-25 20:19:09 +0530

    partial Application of functions

--- Objects-Master-List-of-Problems-Super-Useful-Daily-Techniques/Lodash_Usefull_Techniques/pick-partialRight_Only_cherryPick_Objects.js
+++ Objects-Master-List-of-Problems-Super-Useful-Daily-Techniques/Lodash_Usefull_Techniques/pick-partialRight_Only_cherryPick_Objects.js
@@ -118,3 +118,69 @@ Returns
 
 (Array): Returns the new mapped array.
  */
+
+/* More general explanation on partialRight() -
+
+This is a case of the the fundamental partial application technique of JS. Even more fundamentally, In computer science (https://en.wikipedia.org/wiki/Partial_application), partial application (or partial function application) refers to the process of fixing a number of arguments to a function, producing another function of smaller arity. Given a function {\displaystyle f\colon (X\times Y\times Z)\to N}{\displaystyle f\colon (X\times Y\times Z)\to N}, we might fix (or 'bind') the first argument, producing a function of type {\displaystyle {\text{partial}}(f)\colon (Y\times Z)\to N}{\displaystyle {\text{partial}}(f)\colon (Y\times Z)\to N}.
+
+
+http://benalman.com/news/2012/09/partial-application-in-javascript/#partial-application
+
+Partial application can be described as taking a function that accepts some number of arguments, binding values to one or more of those arguments, and returning a new function that only accepts the remaining, un-bound arguments.
+
+What this means is that, given any arbitrary function, a new function can be generated that has one or more arguments "bound," or partially applied.
+
+JavaScript has the built-in method .bindthat works on functions with any number of arguments and can bind an arbitrary amount of parameters. Its invocation has the following syntax.
+
+function.bind(thisValue, [arg1], [arg2], ...)
+
+It turns function into a new function whose implicit this parameter is this value and whose initial arguments are always as given.
+
+function addition(x, y) {
+   return x + y;
+}
+const plus5 = addition.bind(null, 5)
+plus5(10) // output -> 15
+
+Note: this value does not matter for the (non-method) function addition which is why it is null above.
+
+Partial application is the act of taking a function which takes multiple arguments, and “locking in” some of those arguments, producing a function which takes fewer arguments.
+
+#### Further Reading
+https://medium.com/@JosephJnk/partial-function-application-in-javascript-and-flow-7f3ca87074fe
+
+For instance, say we have this function:
+
+function power(exponent: number, base: number) : number {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result = result * base;
+  }
+  return result;
+}
+
+
+We can use this to raise numbers to some power, like doing power(2, 3) to see that 3 squared is 9, or power(3, 2) to see that 2 cubed is 8. It would be more readable to just say square(3) or cube(2), though. To do this, we want to produce a function that’s the result of “locking-in” 2 and 3 to power. We can do this by manually declaring new functions:
+
+const square = x => power(2, x)
+const cube = x => power(3, x)
+
+This is partial application: we create square by making a new function, which just passes its arguments through to another function, adding some hard-coded arguments to that other function. We could be more explicit in our intent by making a partialApply function that produces these new functions for us:
+
+const square = partialApply(power, 2);
+const cube = partialApply(power, 3);
+
+This partialApply function is quite easy to write, because it turns out that partial application is baked into JavaScript:
+
+function partialApply (fn, ...args) {
+  return fn.bind(null, ...args)
+}
+
+Note, the arguments (i.e. ...args) that I am passing will start from left to right in the function that will be invoked (ie. fn) So in this case of
+
+const square = x => partialApply(power, 2)
+
+The number 2 will be applied from left to right, i.e. 2 becomes the first argument (exponent ) to the the power() function
+
+
+*/
