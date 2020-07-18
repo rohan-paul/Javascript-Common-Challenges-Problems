c5825f90ff834b740686b8f1400f3173f8c539a2
paul <rohanpaul2@gmail.com>
2020-07-18 14:57:05 +0530

    return array with unique elements in On time

--- General-JS-Problems/remove-duplicate-from-Array-O(n).js
+++ General-JS-Problems/remove-duplicate-from-Array-O(n).js
@@ -36,4 +36,35 @@ https://stackoverflow.com/questions/45742030/push-method-not-available-in-reduce
 
 "This is because Array.prototype.push() returns the new length of the array, not the array itself. Your code will run through one iteration of your reducer, set the accumulative value to an integer, and then fail on the next iteration."
 
+
+Further Reading
+
+1. https://stackoverflow.com/a/53526999/1902852
+
 */
+
+// ********************************************
+
+// Another even more simpler Implementation of above with O(n) performance
+
+const getUnique = (array) => {
+
+  // below variable will keep track of already seen numbers in an object,
+  // because checking if an object has a certain key is cheap (it takes O(1) time).
+  let objWithOnlyUniqueArrayElements = {}
+
+  return array.filter((elem) => !(objWithOnlyUniqueArrayElements[elem] = elem in objWithOnlyUniqueArrayElements))
+}
+
+const a1 = [5, 6, 0, 4, 9, 2, 3, 5, 0, 3, 4, 1, 5, 4, 9]
+console.log(getUnique(a1))
+
+/*
+[
+  5, 6, 0, 4,
+  9, 2, 3, 1
+]
+
+Explanation - 
+ */
+
