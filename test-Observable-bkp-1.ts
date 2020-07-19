f465b841db7290074e3057e6baefb4f136784d23
paul <rohanpaul2@gmail.com>
2020-07-19 14:14:18 +0530

    removing extra test files

--- test-Observable-bkp-1.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from "rxjs"
-
-let a = new Observable((observer) => {
-  console.log(1)
-  observer.next(2)
-})
-a.subscribe((result) => console.log(result))
-a.subscribe((result) => console.log(result))
