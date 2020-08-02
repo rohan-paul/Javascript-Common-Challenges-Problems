cc74b80e35442461b73878bf41fdb2be5868d418
paul <rohanpaul2@gmail.com>
2020-08-03 02:20:05 +0530

    updating some extra code

--- String/determine-if-string-is-unique.py
+++
@@ -1,9 +0,0 @@
-def if_no_duplicates(string):
-	letters = {}
-	for letter in string:
-		if letter in letters:
-			return False
-		letters[letter] = True
-	return True
-
-print(if_no_duplicates('Rohan'))
